ec457b8c10549fd20d533aa862c0cc0ac4793d59
LuanaTecnologia <00001126828725SP@al.educacao.sp.gov.br>
2024-11-06 08:35:29 -0300

    ...

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'animais',
+    'qual o maior animal do mundo?', 
+    'a baleia-azul'
+)
+
+criaCartao(
+    'animais',
+    'qual o menor animal do mundo?',
+    'sapinho-pulga'
+)
+
+criaCartao(
+    'jujutsu kaisen',
+    'quem e o vilao de jujutsu kaisen?',
+    'ryomen sukuna'
+)
+
+criaCartao(
+    'one piece',
+    'qual epsidio o ace de one piece morre?',
+    'episodio 483'
+)
